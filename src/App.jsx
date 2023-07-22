d0b7cb13222f1f59daf3bca7ae0fc61002e77cb2
anishere <tranbinhan1100@gmail.com>
2023-07-22 16:41:30 +0700

    test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,33 @@ import Login from './pages/login'
 import Cart from './pages/cart'
 import Wishlist from './pages/wishlist'
 
+// deploy
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAq7avlxOvSeJNjlOCak9jQE1QBsTIfs5g",
+  authDomain: "ecommerceapp-5b40c.firebaseapp.com",
+  projectId: "ecommerceapp-5b40c",
+  storageBucket: "ecommerceapp-5b40c.appspot.com",
+  messagingSenderId: "477216598237",
+  appId: "1:477216598237:web:2d7c9c3ea65ed0c9439334",
+  measurementId: "G-H1QYKLZ338"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
+
+//deploy end
+
 function App() {
 
   return (
